2a711ab57fd5c038e21180885dcf174e3e603112
cristiansalles <cristiansallesdev@gmail.com>
2025-10-07 21:51:08 -0300

    GITANDO

--- src/main/java/cris/Spring10x/CadastroNinjas/CadastroNinjasApplication.java
+++ src/main/java/cris/Spring10x/CadastroNinjas/CadastroNinjasApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroNinjasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroNinjasApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(CadastroNinjasApplication.class, args);
 
+        System.out.println("Gitando");
+
+    }
 }
