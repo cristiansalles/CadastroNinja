c68f69481fa469cd78b3b6db6c7520f1760d2491
cristiansalles <cristiansallesdev@gmail.com>
2026-01-14 15:10:55 -0300

    fix: Incluir a anotation @JsonIgnore para previnir erro de seriliaa§lizÃalização.

--- src/main/java/cris/Spring10x/CadastroNinjas/Missoes/MissoesModel.java
+++ src/main/java/cris/Spring10x/CadastroNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package cris.Spring10x.CadastroNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import cris.Spring10x.CadastroNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.*;
@@ -22,6 +23,7 @@ public class MissoesModel {
     private String hanck;
     //TODO: Uma missão pode ter varios ninjas.
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore //TODO: interrompe serialização(para o lupe).
     private List<NinjaModel> ninja;
 
 }
