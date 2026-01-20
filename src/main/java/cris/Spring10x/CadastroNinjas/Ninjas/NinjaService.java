36b2de4f42b1641efadfdff11ad6d4cf93bfc215
cristiansalles <cristiansallesdev@gmail.com>
2026-01-20 08:42:56 -0300

    modificado os comentario

--- src/main/java/cris/Spring10x/CadastroNinjas/Ninjas/NinjaService.java
+++ src/main/java/cris/Spring10x/CadastroNinjas/Ninjas/NinjaService.java
@@ -8,35 +8,35 @@ import java.util.Optional;
 @Service
 
 public class NinjaService {
-    //TODO: instanciar repositorio.
+    //instanciar repositorio.
     private NinjaRepository ninjaRepository;
 
     public NinjaService(NinjaRepository ninjaRepository) {
         this.ninjaRepository = ninjaRepository;
     }
 
-    //TODO: Listar ninjas.
+    //Listar ninjas.
     public List<NinjaModel> listarNinjas()  {
         return ninjaRepository.findAll();
     }
 
-    //TODO: listar todos por ID.
+    //listar todos por ID.
     public NinjaModel listarNinjasPorId(long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
     }
 
-    //TODO: Criar novo ninja.
+    //Criar novo ninja.
     public NinjaModel criarNinja(NinjaModel ninja) {
         return ninjaRepository.save(ninja);
     }
 
-    //TODO: Deletar Ninjas.
+    //Deletar Ninjas.
     public void deletarNinja(Long id) {
         ninjaRepository.deleteById(id);
     }
 
-    //TODO: Alterar Cadastro.
+    //Alterar Cadastro.
     public NinjaModel alterarNinja (Long id, NinjaModel ninjaAlterado) {
         if (ninjaRepository.existsById(id)) {
             ninjaAlterado.setId(id);
